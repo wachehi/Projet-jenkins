3bc350f4099ac9bf2cb489273aa6a2e0cff23df8
wachehi <wachehi@hotmail.com>
2021-08-16 13:36:57 +0200

    Ajout des nouveaux test Junit

--- cinema/src/test/java/org/sid/cinema/CinemaApplicationTests.java
+++ cinema/src/test/java/org/sid/cinema/CinemaApplicationTests.java
@@ -1,5 +1,7 @@
 package org.sid.cinema;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,20 @@ class CinemaApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	 public void asserThanOnePlusOneIsTwo() {
+		int somme = 1 + 1;
+		assertThat(somme).isNotNull();
+		assertThat(somme).isEqualTo(2);
+	}
+	
+	@Test
+	 public void asserThanOnePlusTwoIsThree() {
+		int somme = 2 + 1;
+		assertThat(somme).isNotNull();
+		assertThat(somme).isEqualTo(3);
 	}
 
 }
